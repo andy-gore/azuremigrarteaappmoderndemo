c6415430cbb15363c47f49863f8a639c3fe7f3ad
andy-gore <64426235+andy-gore@users.noreply.github.com>
2021-06-29 12:24:28 +0000

    aks cluster build

--- build-aks.sh
+++ build-aks.sh
@@ -1,2 +1,26 @@
-RESOURCE_GROUP = migrate-aks-rg
-az group create --name $RESOURCE_GROUP
+RESOURCE_GROUP=migrate-aks-rg
+WINDOWS_USERNAME=aksadminuser
+AKS_CLUSTER=akscluster
+ACR=bungle
+
+az group create --name $RESOURCE_GROUP --location westus2
+
+az aks create \
+    --resource-group $RESOURCE_GROUP \
+    --name $AKS_CLUSTER \
+    --node-count 1 \
+    --enable-addons monitoring \
+    --generate-ssh-keys \
+    --windows-admin-username $WINDOWS_USERNAME \
+    --vm-set-type VirtualMachineScaleSets \
+    --kubernetes-version 1.20.5 \
+    --network-plugin azure
+
+az aks nodepool add \
+    --resource-group $RESOURCE_GROUP \
+    --cluster-name $AKS_CLUSTER \
+    --os-type Windows \
+    --name npwin \
+    --node-count 1
+
+az aks update -n $AKS_CLUSTER -g $RESOURCE_GROUP --attach-acr $ACR
